68db4a45b890613d340826a805a5ea617e4dae68
sean <midruss@yahoo.com>
2022-01-24 15:35:03 -0500

    Finished Phase 1

--- skeleton/bin/p01_basic_server.rb
+++ skeleton/bin/p01_basic_server.rb
@@ -1,1 +1,27 @@
 require 'rack'
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new 
+    res['Content-Type'] = 'text/html'
+    res.write("Hello world!")
+    res.finish 
+end
+
+Rack::Server.start(
+    app: app,
+    Port: 3000
+)
+
+app = Proc.new do |env|
+    req = Rack::Request.new(env)
+    res = Rack::Response.new 
+    res['Content-Type'] = 'text/html'
+    res.write(req.path)
+    res.finish 
+end
+
+Rack::Server.start(
+    app: app,
+    Port: 3000
+)
