809dada4c2507468e7dd7cbcaa1a5eb501f0eb46
sean <midruss@yahoo.com>
2022-01-24 18:21:20 -0500

    End of day progress

--- skeleton/lib/controller_base.rb
+++ skeleton/lib/controller_base.rb
@@ -8,20 +8,28 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req 
+    @res = res 
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    #false until self.render_content(content, content_type)
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    res.status = 302
+    res.location = url
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    @res.write(content) 
+    @res["Content_Type"] = content_type
+    nil
   end
 
   # use ERB and binding to evaluate templates
